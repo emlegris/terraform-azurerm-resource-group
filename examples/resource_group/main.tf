3ad315859d57c485e614c534e3e1b69e5647999c
Emmanuel Legris <emmanuel.legris@outlook.com>
2022-12-18 00:26:28 -0500

    Unified example + Create Flag for RG

--- examples/resource_group/main.tf
+++ examples/resource_group/main.tf
@@ -1,11 +1,14 @@
 module "rg" {
   source = "../.."
 
-  prefix_name   = "web"
-  suffix_name   = "ngx"
-  full_env_code = "lab-cc"
-  location      = "canadacentral"
-  create        = true
+  create                = var.create
+  prefix_name           = var.prefix_name
+  suffix_name           = var.suffix_name
+  full_env_code         = var.full_env_code
+  location              = var.location
+  management_lock_level = var.management_lock_level
+  management_lock_notes = var.management_lock_notes
+
   tags = {
     "Environment" = "lab"
     "Deployment"  = "terraform"
